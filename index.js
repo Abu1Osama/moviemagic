3ec121313c88c5036ebec9a17e6519848e1c13f1
Abu Osama <112826965+Abu1Osama@users.noreply.github.com>
2023-06-25 01:09:36 +0530

    songs route added

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const connectDB = require('./controllers/db');
-const authRoutes = require('./routes/auth');
-const movieRoutes = require('./routes/movies');
-const serialRoutes = require('./routes/serials');
-const songRoutes = require('./routes/songs'); // Add this line
-const favoriteRoutes = require('./routes/favorites');
-const dotenv = require('dotenv');
-const path = require('path');
+const connectDB = require("./controllers/db");
+const authRoutes = require("./routes/auth");
+const movieRoutes = require("./routes/movies");
+const serialRoutes = require("./routes/serial");
+const songRoutes = require("./routes/Song"); // Add this line
+const favoriteRoutes = require("./routes/favorites");
+const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 connectDB();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, "uploads")));
 
-app.use('/api/auth', authRoutes);
-app.use('/api/movies', movieRoutes);
-app.use('/api/serials', serialRoutes);
-app.use('/api/songs', songRoutes); // Add this line
-app.use('/api/favorites', favoriteRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/movies", movieRoutes);
+app.use("/api/serials", serialRoutes);
+app.use("/api/songs", songRoutes); // Add this line
+app.use("/api/favorites", favoriteRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
