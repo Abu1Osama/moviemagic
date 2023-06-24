29344bd6a8bc90c8cc42c3deca45d5a3784e5a0b
Abu Osama <112826965+Abu1Osama@users.noreply.github.com>
2023-06-25 02:17:59 +0530

    songs route added

--- routes/Song.js
+++ routes/Song.js
@@ -4,28 +4,34 @@ const Song = require("../models/Song");
 
 // Route for creating a new song
 router.post("/", async (req, res) => {
-  try {
-    const { title, artist, duration, releaseYear, video_id, Singer } = req.body;
-
-    const song = new Song({
-      title,
-      artist,
-      duration,
-      releaseYear,
-      video_id,
-      Singer,
-    });
-
-    await song.save();
-
-    res.status(201).json({ message: "Song created successfully." });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ error: "An error occurred while creating the song." });
-  }
-});
-
+    try {
+      const { title, artist, duration, releaseYear, video_id, Singer } = req.body;
+  
+      // Check if the video ID already exists
+      const existingSong = await Song.findOne({ video_id });
+      if (existingSong) {
+        return res.status(400).json({ error: "Video ID already exists." });
+      }
+  
+      const song = new Song({
+        title,
+        artist,
+        duration,
+        releaseYear,
+        video_id,
+        Singer,
+      });
+  
+      await song.save();
+  
+      res.status(201).json({ message: "Song created successfully." });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "An error occurred while creating the song." });
+    }
+  });
+  
 // Route for getting all songs
 router.get("/", async (req, res) => {
   try {
