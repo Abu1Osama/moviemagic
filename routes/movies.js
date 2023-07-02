dfbd0c45c7b6c722361089c7cf04550596a356d0
Abu Osama <112826965+Abu1Osama@users.noreply.github.com>
2023-07-02 20:25:30 +0530

    private route removed

--- routes/movies.js
+++ routes/movies.js
@@ -74,7 +74,7 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.patch("/:id", async (req, res) => {
+router.put("/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const {
