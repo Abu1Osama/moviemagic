d875ad57566373ab8211b42c64596d4be0a4a526
Abu Osama <112826965+Abu1Osama@users.noreply.github.com>
2023-07-02 20:16:07 +0530

    private route removed

--- routes/movies.js
+++ routes/movies.js
@@ -1,11 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authMiddleware = require('../middlewares/auth');
-const Movie = require('../models/Movie');
+const authMiddleware = require("../middlewares/auth");
+const Movie = require("../models/Movie");
 
-router.post('/', authMiddleware.verifyToken, async (req, res) => {
+router.post("/", async (req, res) => {
   try {
-    const { video_id, trailer_id, name, genre, director, cast, rating, year_of_release, poster, details, duration } = req.body;
+    const {
+      video_id,
+      trailer_id,
+      name,
+      genre,
+      director,
+      cast,
+      rating,
+      year_of_release,
+      poster,
+      details,
+      duration,
+    } = req.body;
 
     const movie = new Movie({
       video_id,
@@ -23,72 +35,106 @@ router.post('/', authMiddleware.verifyToken, async (req, res) => {
 
     await movie.save();
 
-    res.status(201).json({ message: 'Movie created successfully.' });
+    res.status(201).json({ message: "Movie created successfully." });
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred while creating the movie.' });
-    console.log(error)
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the movie." });
+    console.log(error);
   }
 });
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const movies = await Movie.find();
 
     res.json({ movies });
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred while retrieving the movies.' });
+    res
+      .status(500)
+      .json({ error: "An error occurred while retrieving the movies." });
   }
 });
 
-router.get('/:id', authMiddleware.verifyToken, async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const { id } = req.params;
 
     const movie = await Movie.findById(id);
 
     if (!movie) {
-      return res.status(404).json({ error: 'Movie not found.' });
+      return res.status(404).json({ error: "Movie not found." });
     }
 
     res.json({ movie });
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred while retrieving the movie.' });
+    res
+      .status(500)
+      .json({ error: "An error occurred while retrieving the movie." });
   }
 });
 
-router.patch('/:id', authMiddleware.verifyToken, async (req, res) => {
+router.patch("/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const { video_id, trailer_id, name, genre, director, cast, rating, year_of_release, poster, details, duration } = req.body;
+    const {
+      video_id,
+      trailer_id,
+      name,
+      genre,
+      director,
+      cast,
+      rating,
+      year_of_release,
+      poster,
+      details,
+      duration,
+    } = req.body;
 
     const movie = await Movie.findByIdAndUpdate(
       id,
-      { video_id, trailer_id, name, genre, director, cast, rating, year_of_release, poster, details, duration },
+      {
+        video_id,
+        trailer_id,
+        name,
+        genre,
+        director,
+        cast,
+        rating,
+        year_of_release,
+        poster,
+        details,
+        duration,
+      },
       { new: true }
     );
 
     if (!movie) {
-      return res.status(404).json({ error: 'Movie not found.' });
+      return res.status(404).json({ error: "Movie not found." });
     }
 
-    res.json({ message: 'Movie updated successfully.' });
+    res.json({ message: "Movie updated successfully." });
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred while updating the movie.' });
+    res
+      .status(500)
+      .json({ error: "An error occurred while updating the movie." });
   }
 });
 
-router.delete('/:id', authMiddleware.verifyToken, async (req, res) => {
+router.delete("/:id",  async (req, res) => {
   try {
     const { id } = req.params;
 
     const movie = await Movie.findByIdAndDelete(id);
     if (!movie) {
-      return res.status(404).json({ error: 'Movie not found.' });
+      return res.status(404).json({ error: "Movie not found." });
     }
 
-    res.json({ message: 'Movie deleted successfully.' });
+    res.json({ message: "Movie deleted successfully." });
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred while deleting the movie.' });
+    res
+      .status(500)
+      .json({ error: "An error occurred while deleting the movie." });
   }
 });
 
